ca25d62ef89bd448bdc645577a1cfd68000d3b58
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-07-17 14:41:22 -0400

    pictures added to directory

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,32 @@ import Header from './components/Header';
 import WatchList from './components/WatchList';
 import CoinChange from './components/CoinChange';
 import CoinMenu from './components/CoinMenu';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  fetch("https://api.coincap.io/v2/assets")
-    .then(response => response.json())
-    .then(cryptoObject => { console.log(cryptoObject) })
+  const [cryptoArray, setCryptoArray] = useState([]);
 
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then(response => response.json())
+      .then(cryptoObject => {
+        const fetchedArray = cryptoObject.data;
+        fetchedArray.forEach(coin => {
+          coin.imgSource = `./assets/images/${coin.symbol.toLowerCase()}.png`
+        });
+        setCryptoArray(fetchedArray);
+        console.log(fetchedArray);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className="app">
       <Header />
-      <CoinMenu />
+      <CoinMenu cryptoArray={cryptoArray} />
       <CoinDetails />
       <CoinChange />
       <WatchList />
