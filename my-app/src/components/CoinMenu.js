e3e3f1e9e70d688857c23339e210973be1f163cb
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-07-17 14:41:04 -0400

    CoinMenu rendering x amount of elements/buttons functionality added

--- my-app/src/components/CoinMenu.js
+++ my-app/src/components/CoinMenu.js
@@ -1,5 +1,65 @@
-export default function CoinMenu() {
+import React, { useState } from 'react';
+
+export default function CoinMenu({ cryptoArray }) {
+    const [currentPage, setCurrentPage] = useState(0);
+    const coinsPerPage = 10;
+
+    const createCoinMenuItem = (symbol, name, price, imgSource) => {
+        const parsedPrice = parseFloat(price);
+
+        return (
+            <li className="coin-item" key={symbol}>
+                <p className="coin-name-symbol">{`${name} (${symbol})`}</p>
+                <img
+                    className="coin-image"
+                    src={imgSource}
+                    onError={(e) => {
+                        e.target.src = "https://i.ibb.co/zRLmrMN/shitcoin.png";
+                    }}
+                    alt={symbol}
+                />
+                <p className="coin-price">{`Real-Time Price: $${parsedPrice.toFixed(2)}`}</p>
+            </li>
+        );
+    };
+
+    const renderCoinMenuItems = () => {
+        const start = currentPage * coinsPerPage;
+        const end = start + coinsPerPage;
+        const displayedCoins = cryptoArray.slice(start, end);
+
+        return displayedCoins.map((coin) => {
+            const { symbol, name, priceUsd } = coin;
+            return createCoinMenuItem(symbol, name, priceUsd);
+        });
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        const totalPages = Math.ceil(cryptoArray.length / coinsPerPage);
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
     return (
-        "This is the CoinMenu section test"
-    )
-};
+        <div className='coin-menu'>
+            <ul className="coin-list">
+                {renderCoinMenuItems()}
+            </ul>
+            <div className="pagination-buttons">
+                <button onClick={handlePreviousPage} disabled={currentPage === 0}>
+                    Previous
+                </button>
+                <button onClick={handleNextPage} disabled={currentPage === Math.ceil(cryptoArray.length / coinsPerPage) - 1}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
