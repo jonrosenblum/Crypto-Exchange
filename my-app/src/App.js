a0dc4cdebb9f5c3f0838a6e7908fbf2119a01dea
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-07-17 13:01:06 -0400

    testing git with Victor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,30 @@
-import Header from "./components/Header";
-import CoinMenu from "./components/CoinMenu";
-import CoinDetails from "./components/CoinDetails";
-import CoinChange from "./components/CoinChange";
-import WatchList from "./components/WatchList";
+import React, { useEffect, useState } from 'react';
+import CoinDetails from './components/CoinDetails';
+import Header from './components/Header';
+import WatchList from './components/WatchList';
+import CoinChange from './components/CoinChange';
+import CoinMenu from './components/CoinMenu';
+import { fetchCryptoData } from './api';
 
 function App() {
+  const [cryptoData, setCryptoData] = useState([]);
+
+  useEffect(() => {
+    fetchCryptoData()
+      .then(data => setCryptoData(data))
+      .catch(error => {
+        console.error("Error: ", error)
+      });
+  }, []);
+
   return (
-    <>
+    <div className="app">
       <Header />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <CoinMenu />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <CoinDetails />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <CoinChange />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <WatchList />
-    </>
+      <CoinMenu cryptoData={cryptoData} />
+      <CoinDetails cryptoData={cryptoData} />
+      <CoinChange cryptoData={cryptoData} />
+      <WatchList cryptoData={cryptoData} />
+    </div>
   );
 }
 
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,26 @@
-export default function Header() {
+import React from 'react';
+
+function Header() {
     return (
-        "This is the Header Section"
-    )
-};
+        <header>
+
+            <h1>Crypto Tracker React Version!</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <div className="signup-form">
+                <button>Login</button>
+                <button>Signup</button>
+            </div>
+        </header >
+    );
+}
+
+export default Header;
+
+
+
