698b387bd59aded27410ab3a7e61fa3190bc10b3
Victor Torres <victor.fmt@gmail.com>
2023-07-19 14:04:27 -0400

    public images icons and main graphic css

--- my-app/src/components/routes/Welcome.jsx
+++ my-app/src/components/routes/Welcome.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
+//import ".src/index.css";
 
 export default function Welcome() {
   return (
     <div>
-      <p>
+      <p className="welcome-p">
         Welcome to our cryptocurrency tracker app! Stay updated with real-time
         information about various cryptocurrencies, including their prices,
         market trends, and other relevant data. Explore the world of digital
         currencies and make informed investment decisions. Start tracking your
         favorite cryptocurrencies now!
       </p>
+      <div className="main-graphic">
+        <img src="public/images/icons/main-graphic.png" alt="main graphic"/>
+      </div>
       <Outlet />
     </div>
   );
