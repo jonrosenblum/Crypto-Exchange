aaf92d65c023a00fc9b5d2ff09dc76649551b556
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-20 21:19:50 -0400

    Refactoring

--- my-app/src/components/Layout/Header.jsx
+++ my-app/src/components/Layout/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
 import Nav from "./Nav.jsx";
 
 export default function Header() {
   return (
-  <>
-  <Nav/>
-  </>)};
+    <>
+      <Nav/>
+    </>
+    )
+  };
    
--- my-app/src/routes/Welcome/index.jsx
+++ my-app/src/routes/Welcome/index.jsx
@@ -1,54 +1,13 @@
 import React from "react";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { MDBContainer } from "mdb-react-ui-kit";
+import Nav from "../../components/Layout/Nav";
 
 export default function Welcome() {
   return (
-    // <div
-    //   id="home"
-    //   className="relative overflow-hidden pt-[120px] md:pt-[130px] lg:pt-[160px]"
-    // >
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4">
-            <div
-              className="hero-content wow fadeInUp mx-auto max-w-[780px] text-center"
-              data-wow-delay=".2s"
-            >
-              <h1 class="text-4xl font-bold tracking-tight text-dark sm:text-6xl mb-8">Welcome to Cryptox</h1>
-              <p
-                className="text-lg leading-8 text-body-color mb-12"
-              >
-                Stay updated with real-time information about various
-                cryptocurrencies, including their prices, market trends, and other
-                relevant data.
-                Explore the world of digital currencies and make informed investment
-                decisions. Start tracking your favorite cryptocurrencies now!
-              </p>
-              <ul className="mb-12 flex flex-wrap items-center justify-center">
-                <li>
-                  <a
-                    href=""
-                    className="inline-flex items-center justify-center rounded-lg font-semibold bg-primary py-4 px-6 text-center text-base text-white transition duration-300 ease-in-out hover:text-white hover:shadow-lg sm:px-10"
-                  >
-                    Get started
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href=""
-                    target="_blank"
-                    className="font-semibold leading-6 text-dark transition duration-300 ease-in-out hover:opacity-70 sm:px-10"
-                  >
-                    Login
-                    <span className="pl-2">→</span>
-                  </a>
-                </li>
-              </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      // </div>
+    <MDBContainer className="home-page">
+      <>Hello</>
+    </MDBContainer>
   );
 }
